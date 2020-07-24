d6a05c38ef0763fefac5da2c2bd4cb0b49310813
Aman0802 <aman.khubani@gmail.com>
2020-07-24 19:33:25 +0530

    implemented redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,21 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-// import { persistStore } from "redux-persist";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
 // the middleware that the store is expecting from redux is going to be an array
 const middlewares = [];
 
-if (process.env.NODE_ENV === "developement") {
+if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
 //createStore gets the reducer and the return value of apply middleware
 // applyMiddleware(...middlewares): what this will do is it will copy all the values in the array as individual arguments to the function applyMiddleware
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-// const persistor = persistStore(store);
+export const persistor = persistStore(store);
 
-// export default { store, persistor };
-export default store;
+export default { store, persistStore };
+// export default store;
